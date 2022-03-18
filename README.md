c86833c58d2f930eb5da1291d43ce251cbcc266e
Alessandro Borione <leax_xiv@me.com>
2022-03-18 17:36:53 +0100

    Add -bv in/out mode

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ strict digraph {
 
 ## *-v*
 
-First line contains the number of verteces `nV` (labelled `0` - `nV-1`).  
+First line contains the number of verteces *n<sub>V</sub>* (labelled 0 - *n<sub>V</sub>*-1).  
 Verteces with no outgoing edge **MUST** be included.
 
 ```
@@ -38,6 +38,39 @@ Verteces with no outgoing edge **MUST** be included.
 0	3
 1	2
 2	0
+```
+
+## *-bv*
+
+64-bit signed int little eendian encoding of graph.
+
+**HEADER** (16 bytes):
+- *n<sub>V</sub>* (64 MSB)
+- *n<sub>Emax</sub>* (64 LSB)
+
+**BODY** (8 * (*n<sub>Emax</sub>* + 1) bytes) repeated *n<sub>V</sub>* times:
+- vertex laber *v<sub>i</sub>* (64 MSB)
+- *n<sub>Emax</sub>* adjecent verteces *w<sub>i</sub>* (64 * *n<sub>Emax</sub>* LSB)
+  - if *v<sub>i</sub>* has *n<sub>E</sub> < n<sub>Emax</sub>* adjacent vertices, the remaining *p = 8 * (n<sub>Emax</sub> - n<sub>E</sub>)* bytes are padded with `0xFF`
+
+
+```
+        00 01 02 03 04 05 06 07
+
+0x00	04 00 00 00 00 00 00 00
+0x08	02 00 00 00 00 00 00 00
+0x10	00 00 00 00 00 00 00 00
+0x18	01 00 00 00 00 00 00 00
+0x20	03 00 00 00 00 00 00 00
+0x28	01 00 00 00 00 00 00 00
+0x30	02 00 00 00 00 00 00 00
+0x38	FF FF FF FF FF FF FF FF
+0x40	02 00 00 00 00 00 00 00
+0x48	00 00 00 00 00 00 00 00
+0x50	FF FF FF FF FF FF FF FF
+0x58	03 00 00 00 00 00 00 00
+0x60	FF FF FF FF FF FF FF FF
+0x68	FF FF FF FF FF FF FF FF
 ```
 
 ---
@@ -79,4 +112,41 @@ First line contains the number of verteces `nV` (labelled `0` - `nV-1`).
 2	0
 2	1
 3	0
+```
+
+## *-bv*
+
+64-bit signed int little eendian encoding of graph.
+
+**HEADER** (16 bytes):
+- *n<sub>V</sub>* (64 MSB)
+- *n<sub>Emax</sub>* (64 LSB)
+
+**BODY** (8 * (*n<sub>Emax</sub>* + 1) bytes) repeated *n<sub>V</sub>* times:
+- vertex laber *v<sub>i</sub>* (64 MSB)
+- *n<sub>Emax</sub>* adjecent verteces *w<sub>i</sub>* (64 * *n<sub>Emax</sub>* LSB)
+  - if *v<sub>i</sub>* has *n<sub>E</sub> < n<sub>Emax</sub>* adjacent vertices, the remaining *p = 8 * (n<sub>Emax</sub> - n<sub>E</sub>)* bytes are padded with `0xFF`
+
+
+```
+        00 01 02 03 04 05 06 07
+
+0x00	04 00 00 00 00 00 00 00
+0x08	03 00 00 00 00 00 00 00
+0x10	00 00 00 00 00 00 00 00
+0x18	01 00 00 00 00 00 00 00
+0x20	02 00 00 00 00 00 00 00
+0x28	03 00 00 00 00 00 00 00
+0x30	01 00 00 00 00 00 00 00
+0x38	00 00 00 00 00 00 00 00
+0x40	02 00 00 00 00 00 00 00
+0x48	FF FF FF FF FF FF FF FF
+0x50	02 00 00 00 00 00 00 00
+0x58	00 00 00 00 00 00 00 00
+0x60	01 00 00 00 00 00 00 00
+0x68	FF FF FF FF FF FF FF FF
+0x70	03 00 00 00 00 00 00 00
+0x78	00 00 00 00 00 00 00 00
+0x80	FF FF FF FF FF FF FF FF
+0x88	FF FF FF FF FF FF FF FF
 ```
--- gconv.py
+++ gconv.py
@@ -4,14 +4,50 @@ import sys
 from enum import Flag, auto
 
 
-class Settings(Flag):
+class Setting(Flag):
     NIL = 0
     IN_V = auto()
     IN_E = auto()
+    IN_BV = auto()
     OUT_V = auto()
     OUT_E = auto()
+    OUT_BV = auto()
     UNDIR = auto()
     SORT = auto()
+
+
+def inputVertexList(lines):
+    result = {}
+    indexmap = {}
+    nV, *lines = lines
+    try:
+        nV = int(nV)
+    except ValueError as e:
+        if '\t' in e:
+            print(
+                "ERROR: tried to open file with mode -v. Maybe you meant -e?")
+
+    assert nV == len(lines),\
+        f"Wrong number of lines in file. Expected {nV}, found {len(lines)}"
+
+    for i in range(nV):
+        line = lines[i]
+        edges = line.split(":")
+        v, edges = edges
+
+        edges = edges.strip().split(' ')
+        assert edges[-1] == '#', "Last element of line expected to be '#'"
+        edges = edges[:-1]
+        result[str(v)] = list(map(int, edges))
+        indexmap[str(v)] = i
+
+    for edges in result.values():
+        for w in edges:
+            if str(w) not in indexmap:
+                indexmap[str(w)] = nV
+                nV += 1
+
+    return [result, indexmap]
 
 
 def inputEdgeList(lines):
@@ -38,31 +74,31 @@ def inputEdgeList(lines):
     return [result, indexmap]
 
 
-def inputVertexList(lines):
-    result = {}
-    indexmap = {}
-    nV, *lines = lines
-    nV = int(nV)
+def inputBinaryVertex(chunk, result={}, indexmap={}):
+    i = len(indexmap)
+    v = None
 
-    assert nV == len(lines),\
-        f"Wrong number of lines in file. Expected {nV}, found {len(lines)}"
+    for j in range(0, len(chunk), 8):
+        w = int.from_bytes(chunk[j:j + 8], byteorder="little", signed=True)
 
-    for i in range(nV):
-        line = lines[i]
-        edges = line.split(":")
-        v, edges = edges
+        if j == 0:
+            assert w >= 0, f'Expected positive value, found {w}'
 
-        edges = edges.strip().split(' ')
-        assert edges[-1] == '#', "Last element of line expected to be '#'"
-        edges = edges[:-1]
-        result[str(v)] = list(map(int, edges))
-        indexmap[str(v)] = i
+            v = str(w)
+            if v not in indexmap:
+                indexmap[v] = i
+                i += 1
 
-    for edges in result.values():
-        for w in edges:
+            result[v] = []
+        else:
+            if w < 0:
+                break
+
             if str(w) not in indexmap:
-                indexmap[str(w)] = nV
-                nV += 1
+                indexmap[str(w)] = i
+                i += 1
+
+            result[v].append(w)
 
     return [result, indexmap]
 
@@ -141,37 +177,74 @@ def ouputEdgeList(nodeList, indexmap, doSort):
     ])
 
 
+def outputBinaryVertex(nodeList, indexmap, doSort, pad=-1):
+    nEmax = len(max(nodeList.values(), key=len))
+    indexmap['-1'] = -1
+    output = list(len(nodeList).to_bytes(8, 'little', signed=True)) + \
+            list(nEmax.to_bytes(8, 'little', signed=True))
+
+    for v, edges in (sorted(nodeList.items(), \
+                        key=lambda i: indexmap[i[0]]) if doSort \
+                        else nodeList.items()):
+        output += list(int(indexmap[v]).to_bytes(8, 'little', signed=True))
+        edges = (sorted(edges, \
+                    key=lambda e: indexmap[str(e)]) if doSort \
+                    else edges)
+        for w in (edges + [pad] * (nEmax - len(edges))):
+            output += list(indexmap[str(w)].to_bytes(8, 'little', signed=True))
+            yield bytearray(output)
+            output = []
+
+
 def convert(inF, outF, settings):
     nodes = {}
     indexmap = {}
 
-    with open(inF, 'r') as f:
-        print(f"Loading {f.name}...")
+    print(f"Loading {inF}...")
 
-        if settings & Settings.IN_E:
+    assert len(Setting) == 9, "Exhaustive Setting definition"
+    if settings & Setting.IN_E:
+        with open(inF, 'r') as f:
             [nodes, indexmap] = inputEdgeList(f.readlines())
-        elif settings & Settings.IN_V:
+    elif settings & Setting.IN_V:
+        with open(inF, 'r') as f:
             [nodes, indexmap] = inputVertexList(f.readlines())
-        else:
-            print(f"ERROR: inMode not specified")
-            sys.exit(1)
+    elif settings & Setting.IN_BV:
+        with open(inF, 'rb') as f:
+            nV = int.from_bytes(f.read(8), byteorder='little', signed=True)
+            nEmax = int.from_bytes(f.read(8), byteorder='little', signed=True)
+            for _ in range(nV):
+                [nodes, indexmap] = inputBinaryVertex(f.read(8 * (1 + nEmax)),
+                                                      nodes, indexmap)
+    else:
+        print(f"ERROR: inMode not specified")
+        sys.exit(1)
 
     print("Loading complete!")
 
-    if settings & Settings.UNDIR:
+    assert len(Setting) == 9, "Exhaustive Setting definition"
+    if settings & Setting.UNDIR:
         print("Checking graph for direct edges...")
         [nodes, indexmap] = checkUndir(nodes, indexmap)
         print("Graph is undirected!")
 
-    with open(outF, 'w') as f:
-        print(f"Writing output file {f.name}...")
-        if settings & Settings.OUT_E:
-            f.write(ouputEdgeList(nodes, indexmap, settings & Settings.SORT))
-        elif settings & Settings.OUT_V:
-            f.write(ouputVertexList(nodes, indexmap, settings & Settings.SORT))
-        else:
-            print(f"ERROR: outMode not specified")
-            sys.exit(1)
+    print(f"Writing output file {outF}...")
+
+    assert len(Setting) == 9, "Exhaustive Setting definition"
+    if settings & Setting.OUT_E:
+        with open(outF, 'w') as f:
+            f.write(ouputEdgeList(nodes, indexmap, settings & Setting.SORT))
+    elif settings & Setting.OUT_V:
+        with open(outF, 'w') as f:
+            f.write(ouputVertexList(nodes, indexmap, settings & Setting.SORT))
+    elif settings & Setting.OUT_BV:
+        with open(outF, 'wb') as f:
+            for b in outputBinaryVertex(nodes, indexmap,
+                                        settings & Setting.SORT):
+                f.write(b)
+    else:
+        print(f"ERROR: outMode not specified")
+        sys.exit(1)
 
     print("Writing completed!")
 
@@ -184,6 +257,7 @@ def extractArg(args):
 
 
 def printUsage():
+    assert len(Setting) == 9, "Exhaustive Setting definition"
     print("Usage:")
     print(f"{sys.argv[0]} [CMD] | [MODE <graph_in> MODE <graph_out> OPT]")
     print("CMD:")
@@ -191,16 +265,18 @@ def printUsage():
     print("MODE:")
     print("\t-v\tThe file is interpreted as a list of verteces")
     print("\t-e\tThe file is interpreted as a list of edges")
+    print("\t-bv\tThe file is interpreted as binary list ov vertices")
     print("OPT:")
     print("\t-u\tForce the output graph to be undirected")
     print("\t-s\tSort output ascending")
 
 
 def main(args):
-    settings = Settings.NIL
+    settings = Setting.NIL
     inFile = None
     outFile = None
 
+    assert len(Setting) == 9, "Exhaustive Setting definition"
     while len(args) > 0:
         arg, args = extractArg(args)
 
@@ -215,10 +291,10 @@ def main(args):
                 sys.exit(0)
 
             if inFile == None:
-                settings |= Settings.IN_V
+                settings |= Setting.IN_V
                 inFile = filename
             else:
-                settings |= Settings.OUT_V
+                settings |= Setting.OUT_V
                 outFile = filename
         elif arg == '-e':
             try:
@@ -228,15 +304,28 @@ def main(args):
                 sys.exit(0)
 
             if inFile == None:
-                settings |= Settings.IN_E
+                settings |= Setting.IN_E
                 inFile = filename
             else:
-                settings |= Settings.OUT_E
+                settings |= Setting.OUT_E
+                outFile = filename
+        elif arg == '-bv':
+            try:
+                filename, args = extractArg(args)
+            except KeyError:
+                printUsage()
+                sys.exit(0)
+
+            if inFile == None:
+                settings |= Setting.IN_BV
+                inFile = filename
+            else:
+                settings |= Setting.OUT_BV
                 outFile = filename
         elif arg == '-u':
-            settings |= Settings.UNDIR
+            settings |= Setting.UNDIR
         elif arg == '-s':
-            settings |= Settings.SORT
+            settings |= Setting.SORT
         else:
             print(f"WARNING: Unrecognized parameter '{arg}' is being skipped.")
 
